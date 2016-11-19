b87f7c999ec0bfb11d5d6e99d31590f710612464
fflores-uai <francisco.floresfanelli@gmail.com>
2016-11-19 20:20:44 -0300

    Mailer

---
+++ Source/Presentacion.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Presentacion.Test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var result = Servicios.Correo.Mailer.Enviar(
+                subject: "test",
+                body: "subject Test",
+                from: "correossmtp2016@gmail.com",
+                to: "pachu.flores@gmail.com");
+
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
--- Source/Servicios/Correo/Mailer.cs
+++ Source/Servicios/Correo/Mailer.cs
@@ -1,13 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net;
+using System.Net.Mail;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Servicios.Correo
 {
     public static class Mailer
     {
+        public static string Enviar(
+            string body,
+            string subject,
+            string to,
+            string from)
+        {
+            var destino = new MailAddress(to);
+            var origen = new MailAddress(from);
 
+            var mail = new MailMessage(origen, destino)
+            {
+                Subject = subject,
+                Body = body,
+                BodyEncoding = UTF8Encoding.UTF8,
+                Priority = MailPriority.Normal,
+                IsBodyHtml = false,
+                DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure,
+            };
+
+            SmtpClient smtpGmail = new SmtpClient()
+            {
+                Port = 587,
+                Host = "smtp.gmail.com",
+                Credentials = new NetworkCredential("correossmtp2016@gmail.com", "Novedad.01"),
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                DeliveryMethod  = SmtpDeliveryMethod.Network
+            };
+
+            try
+            {
+                smtpGmail.Send(mail);
+                return "OK";
+            }
+            catch (System.Exception ex)
+            {
+                smtpGmail.Dispose();
+                return string.Format("ERROR : {0}", ex);
+            }
+        }
     }
 }
